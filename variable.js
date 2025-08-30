57acedb28631ad96017a122c3d31f93821de33c8
Your Name <you@example.com>
2025-08-30 22:45:51 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,18 @@
+//variable & constants
+let name='sneha';
+let age=25;
+const pi=.1416;
+let name1='mehrima';
+let fullName='mehrima jannat'
+const birthYear=2000;
+const currentYear=2025;
+const age1=currentYear-birthYear;
+console.log(age1)
+
+
+
+console.log(fullName)
+console.log(name1)
+console.log(name);
+console.log(age)
+console.log(pi)
