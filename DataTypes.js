938a47b13f06690bdd40a9ab218bbf9bd4a53cee
Your Name <you@example.com>
2025-09-02 02:31:14 +0600

    update message

---
+++ DataTypes.js
@@ -0,0 +1,42 @@
+ let name='sneara';
+ let job='dveloper';
+ console.log(name,job);
+
+
+ //number
+ let age=25
+ let weight=5.4;
+ console.log(age,weight);
+ //boolen(true/false)
+ let isMarried=false;
+ let isEmployed=true;
+ console.log(isMarried,isEmployed);
+
+ //undefined
+ let x;
+ console.log(x);
+ //null
+ let data=null;
+ console.log(data);
+ //symbol
+ let id=Symbol('123');
+ console.log(id);
+
+ //bigint
+ let n=BigInt(123);
+ console.log(n);
+
+ //object
+ let person={
+    name:'sneara',
+    age:25,
+    isEmployed:true
+
+ };
+   console.log(person)
+
+
+   //array
+   let fruits=['apple','banana','grapes']
+   console.log(fruits);
+
---
+++ practice.js
@@ -0,0 +1,9 @@
+const user={
+    name:'sneara',
+    age:25,
+    isEmployed:true
+};
+
+let fruits=['apple','banana','grapes'];
+
+console.log(user,fruits);
