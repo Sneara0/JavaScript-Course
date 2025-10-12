9400f9b26bd07469a4ffe7e78728f7c43dd9b8c6
Your Name <you@example.com>
2025-10-12 08:44:57 +0600

    add string & StringMethod

---
+++ String.js
@@ -0,0 +1,12 @@
+let name='Sneha';
+let job='Frontend Developer';
+
+
+info=`Hi, iam ${name}, a ${job}.`;
+console.log(info);
+
+let text='javaScript';
+let letter=`${text}.is a programming language`;
+console.log(letter)
+let letter2=`${text} is a programming language`;
+console.log(letter2);
---
+++ StringMethod.js
@@ -0,0 +1,7 @@
+let message='  I love React ';
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+console.log(message.slice(2,6));
+console.log(message.replace('love','like'));
+console.log(message.trim());
+console.log(message.includes('Java'))
